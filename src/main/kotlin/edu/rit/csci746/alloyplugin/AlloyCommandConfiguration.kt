f719ed1d9b5e355e35a0b69f510c6f25f35b10f3
Martin Charles <martincharles07@gmail.com>
2019-12-07 16:05:36 -0500

    Added Persistence

--- src/main/kotlin/edu/rit/csci746/alloyplugin/AlloyCommandConfiguration.kt
+++ src/main/kotlin/edu/rit/csci746/alloyplugin/AlloyCommandConfiguration.kt
@@ -12,6 +12,7 @@ import edu.mit.csail.sdg.alloy4viz.VizGUI
 import edu.mit.csail.sdg.parser.CompUtil
 import edu.mit.csail.sdg.translator.A4Options
 import edu.mit.csail.sdg.translator.TranslateAlloyToKodkod
+import org.jdom.Element
 
 class AlloyCommandConfiguration(
     project: Project,
@@ -56,8 +57,35 @@ class AlloyCommandConfiguration(
             null
         }
 
+    override fun writeExternal(element: Element) {
+        super.writeExternal(element)
+        element.writeString("path", runParams?.filePath)
+        element.writeString("command", runParams?.commandName)
+    }
+
+    override fun readExternal(element: Element) {
+        super.readExternal(element)
+        runParams = RunParams(
+            filePath = element.readString("path") ?: return,
+            commandName = element.readString("command") ?: return
+        )
+    }
+
     data class RunParams(
         val filePath: String,
         val commandName: String
     )
 }
+
+private fun Element.writeString(name: String, value: String?) {
+    val opt = Element("option")
+    opt.setAttribute("name", name)
+    opt.setAttribute("value", value)
+    addContent(opt)
+}
+
+private fun Element.readString(name: String): String? =
+    children
+        .find { it.name == "option" && it.getAttributeValue("name") == name }
+        ?.getAttributeValue("value")
+
